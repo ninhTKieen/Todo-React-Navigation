59473bee8a3b6a6929d93308426babfb6d752381
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-04-17 00:39:50 +0700

    Update auth.model

---
+++ src/models/auth.model.ts
@@ -0,0 +1,43 @@
+export interface ILoginPayload {
+  email: string;
+  password: string;
+}
+
+export interface IUser {
+  userName: string;
+  name: string;
+  surname: string;
+  emailAddress: string;
+  isActive: boolean;
+  fullName: string;
+  phoneNumber: string;
+  lastLoginTime: string;
+  creationTime: string;
+  roleNames: any;
+  homeAddress: string;
+  addressOfBirth: string;
+  dateOfBirth: string;
+  gender: string;
+  nationality: string;
+  profilePictureId: string;
+  imageUrl?: any;
+  phanKhuId: number;
+  houseId: number;
+  identityNumber: any;
+  qrCodeBase64: any;
+  stateFriend: number;
+  id: number;
+}
+
+export interface IAuthState {
+  currentUser?: IUser;
+  isLoggedIn: boolean;
+  accessToken?: string;
+
+  isPendingLoggedIn: boolean;
+  isFetchingUserAPI: boolean;
+}
+
+export interface IToken {
+  accessToken: string;
+}

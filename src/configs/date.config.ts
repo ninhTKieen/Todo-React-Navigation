ffe84ebe59f5cf762c63c204f561bbf6f1060fa4
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-04-16 23:38:58 +0700

    Create configs folder

---
+++ src/configs/date.config.ts
@@ -0,0 +1,24 @@
+export const DAY_OF_WEEK: String[] = [
+  'Sun',
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat',
+];
+
+export const MONTHS: String[] = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];

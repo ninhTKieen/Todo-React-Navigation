1e199bb8d70a27dc9f5d5031ace303cb1f9a19bd
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-04-17 00:58:33 +0700

    Add get user info feature

--- src/features/auth/auth.saga.ts
+++ src/features/auth/auth.saga.ts
@@ -1,12 +1,13 @@
 import {PayloadAction} from '@reduxjs/toolkit';
-import {loginApi} from '@myapp/api/auth.api';
-import {ILoginPayload, IUser} from '@myapp/models/user.model';
-import {call, put, takeLatest, all} from 'redux-saga/effects';
-import {authActions} from './auth.slice';
+
+import authenApi from './auth.api';
+import {ILoginPayload, IUser} from '@myapp/models/auth.model';
+import {call, put, takeLatest, all, select} from 'redux-saga/effects';
+import {authActions, selectAccessToken} from './auth.slice';
 
 function* handleLogin(action: PayloadAction<ILoginPayload>) {
   try {
-    const response: IUser = yield call(loginApi, action.payload);
+    const response: IUser = yield call(authenApi.loginAPI, action.payload);
     //dispatch
     //non-blocking call
     yield put({type: authActions.loginSuccess.type, payload: response});
@@ -15,8 +16,22 @@ function* handleLogin(action: PayloadAction<ILoginPayload>) {
   }
 }
 
+function* handleGetUser() {
+  try {
+    const accessToken: string = yield select(selectAccessToken);
+    const response: IUser = yield call(authenApi.getUserAPI, {accessToken});
+
+    yield put({type: authActions.getUserInfoSuccess, payload: response});
+  } catch (error) {
+    yield put({type: authActions.getUserInfoFailed.type});
+  }
+}
+
 // function* watchingLoginFlow() {}
 
 export default function* authSaga() {
-  yield all([takeLatest(authActions.login.type, handleLogin)]);
+  yield all([
+    takeLatest(authActions.login.type, handleLogin),
+    takeLatest(authActions.getUserInfo.type, handleGetUser),
+  ]);
 }
--- src/features/auth/auth.slice.ts
+++ src/features/auth/auth.slice.ts
@@ -1,9 +1,18 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
-import {IUser, IAuthState, ILoginPayload} from '@myapp/models/user.model';
+import {IAuthState, IUser} from '@myapp/models/auth.model';
+
+// const initialState: IAuthState = {
+//   isLoggedIn: false,
+//   token: '',
+// };
 
 const initialState: IAuthState = {
+  accessToken: undefined,
   isLoggedIn: false,
   currentUser: undefined,
+
+  isPendingLoggedIn: false,
+  isFetchingUserAPI: false,
 };
 
 const authSlice = createSlice({
@@ -11,19 +20,38 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    login(state, action: PayloadAction<ILoginPayload>) {
-      state.isLoggedIn = true;
+    login(state, _action: PayloadAction<{email: string; password: string}>) {
+      state.isPendingLoggedIn = true;
     },
-    loginSuccess(state, action: PayloadAction<IUser>) {
+
+    loginSuccess(state, action: PayloadAction<string>) {
+      state.isPendingLoggedIn = false;
       state.isLoggedIn = true;
-      state.currentUser = action.payload;
+      state.accessToken = action.payload;
     },
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    loginFailed(state, action: PayloadAction<string>) {
+    loginFailed(state) {
+      state.isPendingLoggedIn = false;
       state.isLoggedIn = false;
     },
 
     logout(state) {
+      state.isLoggedIn = false;
+      state.currentUser = undefined;
+    },
+
+    getUserInfo(state) {
+      state.isFetchingUserAPI = true;
+    },
+
+    getUserInfoSuccess(state, action: PayloadAction<IUser>) {
+      state.isFetchingUserAPI = false;
+      state.isLoggedIn = true;
+      state.currentUser = action.payload;
+    },
+
+    getUserInfoFailed(state) {
+      state.isFetchingUserAPI = false;
       state.isLoggedIn = false;
       state.currentUser = undefined;
     },
@@ -36,6 +64,8 @@ export const authActions = authSlice.actions;
 //Selectors
 export const selectIsLoggedIn = (state: any) => state.auth.isLoggedIn;
 export const selectCurrentUser = (state: any) => state.auth.currentUser;
-
+export const selectIsPendingLoggedIn = (state: any) =>
+  state.auth.isPendingLoggedIn;
+export const selectAccessToken = (state: any) => state.auth.accessToken;
 // Reducer
 export default authSlice.reducer;

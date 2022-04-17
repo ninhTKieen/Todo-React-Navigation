b3b3841dfdd6836be448c165a8a3ea134de6bcfe
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-04-17 15:04:33 +0700

    Update navigations

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -5,12 +5,27 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginScreen from '@myapp/screens/Auth/LoginScreen';
 import BottomTabs from './BottomTabs';
 
+import {useAppDispatch} from '@myapp/hooks/redux.hook';
+import {authActions} from '@myapp/features/auth/auth.slice';
 import useCheckAuth from '@myapp/hooks/auth.hook';
 
 const Stack = createNativeStackNavigator();
 
 const Navigator = () => {
   const {isLoggedIn} = useCheckAuth();
+
+  const dispatch = useAppDispatch();
+
+  const {currentUser} = useCheckAuth();
+
+  React.useEffect(() => {
+    const fetchGetMe = () => {
+      !currentUser && dispatch(authActions.getUserInfo());
+    };
+
+    return fetchGetMe();
+  }, [currentUser, dispatch]);
+
   return (
     <Stack.Navigator>
       {!isLoggedIn ? (

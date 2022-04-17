1d6c56b845f2005d04b297c04bca6f3bb7764a3c
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-04-18 00:35:46 +0700

    Update profile screens

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,57 +1,89 @@
 import React from 'react';
 
-import {StyleSheet} from 'react-native';
-import {Surface, Text, Avatar, Title, Subheading} from 'react-native-paper';
+import {StyleSheet, Image, ScrollView} from 'react-native';
+import {Surface, Title, Subheading, Text} from 'react-native-paper';
 
+import {useAppDispatch} from '@myapp/hooks/redux.hook';
+import {authActions} from '@myapp/features/auth/auth.slice';
 import useCheckAuth from '@myapp/hooks/auth.hook';
+import {View} from 'react-native';
 
 const ProfileScreen: React.FC = () => {
+  const dispatch = useAppDispatch();
+
   const {currentUser} = useCheckAuth();
 
-  return (
-    <Surface>
-      <Surface style={styles.container}>
-        <Surface style={styles.info}>
-          <Avatar.Image
-            size={90}
-            source={require('@myapp/assets/Avatar.jpg')}
-            style={styles.avatar}
-          />
-          <Surface>
-            <Title>{currentUser.fullName}</Title>
-            <Subheading>@{currentUser.userName}</Subheading>
-            <Text>{currentUser.dateOfBirth}</Text>
-          </Surface>
-        </Surface>
+  React.useEffect(() => {
+    const fetchGetMe = () => {
+      !currentUser && dispatch(authActions.getUserInfo());
+    };
 
+    return fetchGetMe();
+  }, [currentUser, dispatch]);
+
+  return (
+    <ScrollView>
+      <Surface style={styles.container}>
         <Surface>
-          <Subheading style={styles.quote}>
-            Yesterday is history, tomorrow is a mystery, but today is the gift
-          </Subheading>
+          <Image
+            source={require('@myapp/assets/background.jpeg')}
+            style={styles.background}
+          />
         </Surface>
       </Surface>
-    </Surface>
+
+      <View style={{alignItems: 'center'}}>
+        <Image
+          source={require('@myapp/assets/avatar.webp')}
+          style={styles.avatar}
+        />
+        <Title>{currentUser?.fullName}</Title>
+        <Subheading>@{currentUser?.userName}</Subheading>
+      </View>
+
+      <View style={styles.itemContainer}>
+        <Text>Surname</Text>
+        <Text>{currentUser?.surname}</Text>
+      </View>
+
+      <View style={styles.itemContainer}>
+        <Text>Email</Text>
+        <Text>{currentUser?.userName}@gmail.com</Text>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 20,
+    flex: 1,
   },
 
-  info: {
-    flexDirection: 'row',
+  background: {
+    width: '100%',
+    padding: 10,
+    height: 150,
+    backgroundColor: 'black',
   },
 
   avatar: {
-    marginRight: 20,
-    marginLeft: 20,
+    width: 140,
+    height: 140,
+    borderRadius: 100,
+    marginTop: -60,
   },
 
   quote: {
     marginTop: 20,
     marginLeft: 20,
     marginBottom: 20,
+  },
+
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    padding: 16,
   },
 });
 

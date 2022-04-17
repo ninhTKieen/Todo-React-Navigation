2ad41e032a24782f0f8ef0257aca68d7028094b2
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-04-18 00:38:46 +0700

    Update noti api

--- src/features/notification/noti.api.tsx
+++ src/features/notification/noti.api.tsx
@@ -1,5 +1,9 @@
-import {axiosRequest} from '@myapp/utils/api.utils';
-import {INotiGeneralData, NOTIFICATION_TYPE} from '@myapp/models/noti.model';
+import {axiosMethod, axiosRequest} from '@myapp/utils/api.utils';
+import {
+  INotiGeneralData,
+  IUserCommentData,
+  NOTIFICATION_TYPE,
+} from '@myapp/models/noti.model';
 import {API_END_POINT} from '@env';
 
 class NotificationAPI {
@@ -22,6 +26,18 @@ class NotificationAPI {
       params: {
         type: NOTIFICATION_TYPE.GENERAL,
       },
+    });
+
+    return response.result.data;
+  }
+
+  async getUserComment(noticationId: number): Promise<IUserCommentData[]> {
+    // const url = `${API_END_POINT}/api/services/app/UserCityNotification/GetAllComment`;
+    const url = `${API_END_POINT}/api/services/app/UserCityNotification/GetAllComment?NotifiactionId=${noticationId}`;
+    // truyen params loi ?
+    const response = await axiosRequest({
+      url,
+      method: axiosMethod.GET,
     });
 
     return response.result.data;

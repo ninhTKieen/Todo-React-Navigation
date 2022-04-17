afbc22b2a038b4d35a7fa8a33d822ea427a9561d
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-04-17 12:18:34 +0700

    Add Notification Screen

---
+++ src/screens/Notifications/screens/Apartment/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import {FlatList, View} from 'react-native';
+
+import NotiItem from '@myapp/screens/Notifications/components/NotiItem';
+import notiApi from '@myapp/features/notification/noti.api';
+import {INotiGeneralData} from '@myapp/models/noti.model';
+
+const ApartmentScreen = ({navigation}: any) => {
+  const [items, setItems] = React.useState<INotiGeneralData[]>([]);
+
+  const fetchNotiAPI = async () => {
+    const response = await notiApi.getApartmentlNotiApi();
+    setItems(response);
+  };
+
+  const renderItems = ({item}: any) => {
+    return (
+      <NotiItem
+        name={item.name}
+        countComment={item.countComment}
+        countFollow={item.countFollow}
+        navigate={() => navigation.navigate('Detail')}
+      />
+    );
+  };
+
+  React.useEffect(() => {
+    fetchNotiAPI();
+  }, []);
+
+  return (
+    <View style={{flex: 1}}>
+      <FlatList
+        data={items}
+        renderItem={renderItems}
+        keyExtractor={item => String(item.id)}
+        contentContainerStyle={{flexGrow: 1}}
+      />
+    </View>
+  );
+};
+
+export default ApartmentScreen;
---
+++ src/screens/Notifications/screens/Detail/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+
+const DetailNotiScreen = () => {
+  return (
+    <View>
+      <Text>This is detail screen</Text>
+    </View>
+  );
+};
+
+export default DetailNotiScreen;
